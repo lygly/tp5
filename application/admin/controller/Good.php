caa834d1379772cfddc7bde8f447ef618ec8a12d
lygly <1516045937@qq.com>
2017-07-28 17:53:48 +0800

    图片上传进行中

--- application/admin/controller/Good.php
+++ application/admin/controller/Good.php
@@ -118,7 +118,8 @@ class Good extends Controller
     }
     //保存在数据库
     public function addGoods(){
-      //var_dump(input());
+      /*var_dump(input());
+      exit();*/
       $data['goodsname'] = input('goodsname');
       $data['attributes'] = input('attributes');
       $data['number'] = input('number');
@@ -200,5 +201,25 @@ class Good extends Controller
         $id = input('id');
         $data = Goods::get($id);
         $data->delete();
+    }
+    //上传图片
+    public function upload(){
+
+        // 获取表单上传文件
+        $files = request()->file('image');
+        foreach($files as $file){
+            // 移动到框架应用根目录/public/uploads/ 目录下
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+            if($info){
+                // 成功上传后 获取上传信息
+                // 输出 jpg
+                echo $info->getExtension();
+                // 输出 42a79759f284b767dfcb2a0197904287.jpg
+                echo $info->getFilename();
+            }else{
+                // 上传失败获取错误信息
+                echo $file->getError();
+            }
+        }
     }
 }
