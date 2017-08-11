c36d9aad2f8a97cac9d918f6cbadf63c83dd91f9
lygly <1516045937@qq.com>
2017-08-11 14:47:20 +0800

    验证

--- application/admin/controller/Good.php
+++ application/admin/controller/Good.php
@@ -10,6 +10,7 @@ namespace app\admin\controller;
 
 
 use app\admin\model\Goods;
+use app\admin\model\GoodsFiles;
 use app\admin\model\GoodsType;
 use think\Controller;
 use think\Request;
@@ -133,6 +134,8 @@ class Good extends Controller
       $data['freight'] = input('freight');
       $data['status'] = input('status');
       $data['reorder'] = input('reorder');
+        //拼接图片
+      $data['imagepath']=implode(',', input('imagepath/a'));
       //$data['imagepath'] = input('file');
       $data['text'] = input('text');
       $tid = explode(',', input('tid'));
@@ -142,7 +145,7 @@ class Good extends Controller
       $Goods = new Goods();
       $re = $Goods->save($data);
       if($re){
-          $this->success('成功','Good/goods');
+          $this->success('成功','Good/goods',"",2);
       }else{
           $this->error('失败');
       }
@@ -159,8 +162,16 @@ class Good extends Controller
         }
         //产品详情
         $data = Goods::get($id);
+        //产品图片
+        $picArr = explode(',',$data['imagepath']);
+        $GoodsFile = new GoodsFiles();
+        $img=[];
+        foreach ($picArr as $item){
+            array_push($img,$GoodsFile->get($item));
+        }
         $this->assign('cate',$Cate);
         $this->assign('good',$data);
+        $this->assign('image',$img);
        return $this->fetch('product_edit');
     }
     //更新产品
@@ -184,6 +195,8 @@ class Good extends Controller
         $data['status'] = input('status');
         $data['reorder'] = input('reorder');
         //$data['imagepath'] = input('file');
+        //拼接图片
+        $data['imagepath']=implode(',', input('imagepath'));
         $data['text'] = input('text');
         $tid = explode(',', input('tid'));
         $data['tid'] = $tid[0];
@@ -191,7 +204,7 @@ class Good extends Controller
 
         $re = $data->save();
         if($re){
-            $this->success('成功','Good/goods');
+            $this->success('成功','Good/goods',"",2);
         }else{
             $this->error('失败');
         }
@@ -210,16 +223,33 @@ class Good extends Controller
         foreach($files as $file){
             // 移动到框架应用根目录/public/uploads/ 目录下
             $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+            $date=date("Ymd",time());//已上传日期为子目录名
             if($info){
                 // 成功上传后 获取上传信息
                 // 输出 jpg
-                echo $info->getExtension();
-                // 输出 42a79759f284b767dfcb2a0197904287.jpg
-                echo $info->getFilename();
+                $GoodsFiles = new GoodsFiles();
+                $path =  DS . 'uploads'.'/'.$date.'/'.$info->getFilename();
+                $GoodsFiles->filepath = $path;
+                $GoodsFiles->save();
+                $file = ['id'=>$GoodsFiles->id,'imagepath'=>$path];
+                return json_encode($file);
             }else{
                 // 上传失败获取错误信息
-                echo $file->getError();
+               $this->error($file->getError());
             }
+        }
+    }
+
+
+    //商品图片删除
+    public function product_del_images(){
+        $GoodFile = new GoodsFiles();
+        $data = $GoodFile->get(input('id'));
+        $re = $data->delete();
+        if($re){
+            return 1;
+        }else{
+            return 0;
         }
     }
 }
---
+++ application/admin/model/GoodsFiles.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/7
+ * Time: 10:22
+ */
+
+namespace app\admin\model;
+
+
+use think\Model;
+
+class GoodsFiles extends Model
+{
+
+}
